431631f687451971521785279592ff0e5a2ed03d
avi lugassi <avilugassi23@gmail.com>
2021-06-09 16:28:07 +0300

    avi first commit

---
+++ src/service/family.service.js
@@ -0,0 +1,386 @@
+
+ export default {
+    nodes: [
+      {
+        id: 0,
+        layout: {
+          width: 200,
+          height: 80
+        },
+        familyType: 'MALE',
+        labels: [
+          {
+            text: 'John Fitzgerald\nKennedy'
+          },
+          {
+            text: '* 29 MAY 1917'
+          },
+          {
+            text: '✝ 22 NOV 1963'
+          }
+        ]
+      },
+      {
+        id: 1,
+        layout: {
+          width: 200,
+          height: 80
+        },
+        familyType: 'FEMALE',
+        labels: [
+          {
+            text: 'Jaqueline Lee\nBouvier'
+          },
+          {
+            text: '* 28 JUL 1929'
+          },
+          {
+            text: '✝ 19 MAY 1994'
+          }
+        ]
+      },
+      {
+        id: 2,
+        layout: {
+          width: 200,
+          height: 80
+        },
+        familyType: 'MALE',
+        labels: [
+          {
+            text: 'John Vernou\nBouvier III'
+          },
+          {
+            text: '* 19 MAY 1891'
+          },
+          {
+            text: '✝ 3 AUG 1957'
+          }
+        ]
+      },
+      {
+        id: 3,
+        layout: {
+          width: 200,
+          height: 80
+        },
+        familyType: 'FEMALE',
+        labels: [
+          {
+            text: 'Janet Norton\nLee'
+          },
+          {
+            text: '* 3 DEC 1907'
+          },
+          {
+            text: '✝ 22 JUL 1989'
+          }
+        ]
+      },
+      {
+        id: 4,
+        layout: {
+          width: 200,
+          height: 80
+        },
+        familyType: 'FEMALE',
+        labels: [
+          {
+            text: 'Caroline Bouvier\nKennedy'
+          },
+          {
+            text: '* 27 NOV 1957'
+          }
+        ]
+      },
+      {
+        id: 5,
+        layout: {
+          width: 200,
+          height: 80
+        },
+        familyType: 'MALE',
+        labels: [
+          {
+            text: 'John Fitzgerald\nKennedy'
+          },
+          {
+            text: '* 25 NOV 1960'
+          },
+          {
+            text: '✝ 16 JUL 1999'
+          }
+        ]
+      },
+      {
+        id: 6,
+        layout: {
+          width: 200,
+          height: 80
+        },
+        familyType: 'MALE',
+  
+        labels: [
+          {
+            text: 'Patrick Bouvier\nKennedy'
+          },
+          {
+            text: '* 07 AUG 1963'
+          },
+          {
+            text: '✝ 09 AUG 1963'
+          }
+        ]
+      },
+      {
+        id: 7,
+        layout: {
+          width: 200,
+          height: 80
+        },
+        familyType: 'MALE',
+        labels: [
+          {
+            text: 'Joseph Patrick\nKennedy'
+          },
+          {
+            text: '* 06 SEP 1888'
+          },
+          {
+            text: '✝ 16 NOV 1969'
+          }
+        ]
+      },
+      {
+        id: 8,
+        layout: {
+          width: 200,
+          height: 80
+        },
+        familyType: 'FEMALE',
+        labels: [
+          {
+            text: 'Rose Elizabeth\nFitzgerald'
+          },
+          {
+            text: '* 22 JUL 1890'
+          },
+          {
+            text: '✝ 22 JAN 1995'
+          }
+        ]
+      },
+      {
+        id: 9,
+        layout: {
+          width: 200,
+          height: 80
+        },
+        familyType: 'MALE',
+        labels: [
+          {
+            text: 'Joseph Patrick\nKennedy Jr'
+          },
+          {
+            text: '* 22 JUL 1915'
+          },
+          {
+            text: '✝ 12 AUG 1944'
+          }
+        ]
+      },
+      {
+        id: 10,
+        layout: {
+          width: 200,
+          height: 80
+        },
+        familyType: 'FEMALE',
+        labels: [
+          {
+            text: 'Rosemary\nKennedy'
+          },
+          {
+            text: '* 13 SEP 1918'
+          },
+          {
+            text: '✝ 7 JAN 1918'
+          }
+        ]
+      },
+      {
+        id: 11,
+        layout: {
+          width: 200,
+          height: 80
+        },
+        familyType: 'FEMALE',
+        labels: [
+          {
+            text: 'Kathleen\nKennedy'
+          },
+          {
+            text: '* 20 FEB 1920'
+          },
+          {
+            text: '✝ 13 MAY 1948'
+          }
+        ]
+      },
+      {
+        id: 12,
+        layout: {
+          width: 200,
+          height: 80
+        },
+        familyType: 'FEMALE',
+        labels: [
+          {
+            text: 'Eunice Mary \nKennedy Shriver'
+          },
+          {
+            text: '* 10 JUL 1921'
+          },
+          {
+            text: '✝ 11 AUG 2009'
+          }
+        ]
+      },
+      {
+        id: 13,
+        layout: {
+          width: 200,
+          height: 80
+        },
+        familyType: 'MALE',
+        labels: [
+          {
+            text: 'Robert Sargent\nShriver Jr'
+          },
+          {
+            text: '* 9 NOV 1915'
+          },
+          {
+            text: '✝ 18 JAN 2011'
+          }
+        ]
+      },
+      {
+        id: 14,
+        layout: {
+          width: 200,
+          height: 80
+        },
+        familyType: 'MALE',
+        labels: [
+          {
+            text: 'Aristotle\nOnassis'
+          },
+          {
+            text: '* 20 JAN 1906'
+          },
+          {
+            text: '✝ 15 MAR 1975'
+          }
+        ]
+      },
+   
+    ],
+    edges: [
+      {
+        id: 0,
+        source: 0,
+        target: 15
+      },
+      {
+        id: 1,
+        source: 1,
+        target: 15
+      },
+      {
+        id: 2,
+        source: 15,
+        target: 4
+      },
+      {
+        id: 3,
+        source: 15,
+        target: 5
+      },
+      {
+        id: 4,
+        source: 15,
+        target: 6
+      },
+      {
+        id: 5,
+        source: 7,
+        target: 16
+      },
+      {
+        id: 6,
+        source: 8,
+        target: 16
+      },
+      {
+        id: 7,
+        source: 16,
+        target: 9
+      },
+      {
+        id: 8,
+        source: 16,
+        target: 0
+      },
+      {
+        id: 9,
+        source: 16,
+        target: 10
+      },
+      {
+        id: 10,
+        source: 16,
+        target: 11
+      },
+      {
+        id: 11,
+        source: 16,
+        target: 12
+      },
+      {
+        id: 12,
+        source: 14,
+        target: 17
+      },
+      {
+        id: 13,
+        source: 1,
+        target: 17
+      },
+      {
+        id: 14,
+        source: 2,
+        target: 18
+      },
+      {
+        id: 15,
+        source: 3,
+        target: 18
+      },
+      {
+        id: 16,
+        source: 18,
+        target: 1
+      },
+      {
+        id: 17,
+        source: 13,
+        target: 19
+      },
+      {
+        id: 18,
+        source: 12,
+        target: 19
+      }
+    ]
+  }
